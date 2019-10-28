7b31cfda379dd872cc858c667490c8848dfbc7ae
NanXunbiubiu <37281618+NanXunbiubiu@users.noreply.github.com>
2019-10-28 09:32:06 +0800

    修改接口
    去除一些敏感代码

--- atmosphereWindy/plugin/windy/RequestInterface.js
+++ atmosphereWindy/plugin/windy/RequestInterface.js
@@ -80,42 +80,16 @@
              * @param callback
              */
            getWindData = function(time, item, zoom, callback){
-                time = time == void 0 ? DateUtils.getSmpFormatNowDate() : time; 
-				var UTCtime = DateUtils.getFormatDateHourAdd(time, -8, "yyyyMMddhh") + "00",
-					UTCday = UTCtime.substr(0, 8),
-					UTChour = UTCtime.substr(8, 2);
-				
-				if(item == "temp"){
-					item = "temperature"
-				}else if(item == "humidity"){
-					item = "Humidity"
-				}else if(item == "pressure"){
-					item = "Pressure"
-				}
-				var	url;
-				if(zoom < 5){
-					for(var list = ["00", "03", "06", "09", "12", "15", "18", "21"], p = 0; p < list.length; p++){
-						if(UTChour >= list[p] && UTChour < list[p+1]){
-							UTChour = list[p];
-							break;
-						}
-						if(p === list.length - 1){
-							UTChour = list[p];
-							break;
-						}
-					}
-					
-					url =  "/lidarnet.web/ExtModelData/GFS/json/globe/hour/" + UTCday + "/gfs.pgrb2.1p00.f0" + UTChour + ".glb."+ UTCday +"."+ titleCase(item) +".json"
-				}else{
-					url =  "/lidarnet.web/ExtModelData/GFS/json/china/hour/" + UTCday + "/gfs.pgrb2.0p25.f0" + UTChour + ".chn."+ UTCday +"."+ titleCase(item) +".json"
-				}
-				
-				axios.get(url).then(function (res) {
-					if (res.data) {
-						var data = res.data;				
-						callback(data);
-					}
-				});
+                var url = void 0;
+                url =  '../data/current-' + item + '-surface-level-gfs-1p00.json';
+                axios.get(url, {
+
+                }).then(function (res) {
+                    if (res.data) {
+                        var data = res.data;
+                        callback(data);
+                    }
+                });
             },
             /**
              * getAQIData 获取空气站aqi数据
@@ -125,29 +99,18 @@
              * @param callback
              */
             getAQIData = function(time, type1, type2, callback) {
-               var url;
-				/*if(type1 == "CITY")
-					url = $ctxStatic + '/gbq/modules/air/AtmoWindy/data/cityaqidata.json';
-				else
-					url = $ctxStatic + '/gbq/modules/air/AtmoWindy/data/stationaqidata.json';*/
-				var time1 = void 0 === time ?  ((new Date).getMinutes() < 35 ? new Date((new Date).setHours((new Date).getHours() - 1)).format().substring(0,13) + ":00" : new Date().format().substring(0,13) + ":00") : time;
-				url = $ctx + '/atmoWindy/nationalWindy/getAQIData?time='+ time1 +'&type='+ type1 +'';
-				$.ajax({
-					  type: "get",
-					  url: url,
-					  dataTpye: 'json',
-					  success: function (res) {
-						  res = JSON.parse(res)
-						  if(res.data && res.msg == "success"){
-							  var data = res.data;
-							  callback(data);
-						  }
-					  },
-					  error: function (xhr, type, errorThrown) {
-						console.log("其他异常:", type)
-					  }
-		
-				  })
+              var url;
+                if(type1 == "CITY")
+                    url = '../data/cityaqidata.json';
+                else
+                    url = '../data/stationaqidata.json';
+                axios.get(url, {
+
+                }).then(function(res){
+                    if(res.data && res.data[0].time){
+                        callback(res.data);
+                    }
+                })
             },
             /**
              * getServerData
@@ -157,30 +120,26 @@
              * @param callback
              */
             getServerData = function(path, secret, message, callBack, flag) {
-               var url = void 0,
-					key = void 0;
-				if(secret == "GETFURTUREHISTORY"){
-					//url =  $ctxStatic + '/gbq/modules/air/AtmoWindy/data/futureHistoryData.json'				
-				}else{
-					//url = $ctxStatic + '/gbq/modules/air/AtmoWindy/data/forcast5DaysData.json';
-				}
-				url = $ctx + '/atmoWindy/nationalWindy/getServerData?secret='+ secret +'&cityCode='+message.cityCode+'&item='+ message.item +'&type='+ that._type +'';
-				key = secret + JSON.stringify(message);
-				var data = readDataFromLocalStorage(key, flag);
-				data ? callBack(data.result) : (
-					$.ajax({
-						type: 'get',
-						url: url,
-						success: function(data){
-							if(data = JSON.parse(data),
-									data.result && data.msg == "success"){
-								flag > 0 && (data.time = (new Date).getTime(), storageUtils.localStorageUtil.save(key, data)),
-								callBack(data.result);
-							}
-							
-						}
-					})
-				)
+               var url = void 0;
+                if(secret == "MAPAPI_GETFURTUREHISTORY")
+                    url = '../data/'+ message.item +'FurtureHistoryData.json'
+                else{
+                    url = '../data/forcast5DaysData.json';
+                }
+                var key = secret + JSON.stringify(message);
+                var data = readDataFromLocalStorage(key, flag);
+                data ? callBack(data) : (
+                        $.ajax({
+                            type: 'get',
+                            url: url,
+                            success: function(data){
+                                if(data.result){
+                                    flag > 0 && (data.result.time = (new Date).getTime(), storageUtils.localStorageUtil.save(key, data.result)),
+                                            callBack(data.result);
+                                }
+                            }
+                        })
+                )
             };
 
     return {
